e337735045f6763f507aad8e7990bd31e554bf28
romainberger <romain@romainberger.com>
2014-05-30 12:19:09 +0200

    Comment list

--- js/build/fixtures.js
+++ js/build/fixtures.js
@@ -1,20 +1,26 @@
 var COMMENTS = [
   {
+    "avatar": "http://s2.dmcdn.net/BiIZU/80x80-9pB.jpg",
     "message": "React is pretty cool"
   },
   {
+    "avatar": "http://s1.dmcdn.net/EN3BC/40x40-KU_.jpg",
     "message": "Javascript is pretty cool too"
   },
   {
+    "avatar": "http://s2.dmcdn.net/D-6xW/80x80-w99.jpg",
     "message": "What's up with all this lame comments. My comments is waaaaay better."
   },
   {
+    "avatar": "http://s1.dmcdn.net/EI_Vn/118x118-7Ve.jpg",
     "message": "I'm having problem thinking about fake comments. I should make longer comments to simulate real life."
   },
   {
+    "avatar": "http://s1.dmcdn.net/BYZEy/118x118-R_l.jpg",
     "message": "It's friday and there's like 10 people in the office what am I doing here"
   },
   {
+    "avatar": "http://rberger.dev.dailymotion.com/images/avatar/group/80x80.png.v237717a862157c38",
     "message": "Okay back to react. I should have just lorem ipsum a few comments instead of writing dumb things. Pineapple."
   }
 ]
