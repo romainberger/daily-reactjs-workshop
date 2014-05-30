39ff247aa149ad8d2877b2261607c40cdf86614d
romainberger <romain@romainberger.com>
2014-05-30 11:56:32 +0200

    F*ck it imma use bootstrap

---
+++ js/build/fixtures.js
@@ -0,0 +1,20 @@
+var COMMENTS = [
+  {
+    "message": "React is pretty cool"
+  },
+  {
+    "message": "Javascript is pretty cool too"
+  },
+  {
+    "message": "What's up with all this lame comments. My comments is waaaaay better."
+  },
+  {
+    "message": "I'm having problem thinking about fake comments. I should make longer comments to simulate real life."
+  },
+  {
+    "message": "It's friday and there's like 10 people in the office what am I doing here"
+  },
+  {
+    "message": "Okay back to react. I should have just lorem ipsum a few comments instead of writing dumb things. Pineapple."
+  }
+]
